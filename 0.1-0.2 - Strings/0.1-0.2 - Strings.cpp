5fa30ce796eacc4567ce6d80a2d5598fd8a685f8
Aidan Cuennet <aidan.cuennet@gmail.com>
2021-09-23 16:26:22 +0200

    added first strings assignment

---
+++ 0.1-0.2 - Strings/0.1-0.2 - Strings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+int main()
+{
+    std::string Word = "Inter choice";
+	std::string Replace;
+
+	std::string first_name = "Louis ";
+	std::string last_name = "Udry";
+	std::string age = ", age 19";
+	std::string full_name;
+	full_name = first_name + last_name + age;
+	std::cout << "welcome ! " << full_name << " to this program !" << std::endl;
+	
+}
