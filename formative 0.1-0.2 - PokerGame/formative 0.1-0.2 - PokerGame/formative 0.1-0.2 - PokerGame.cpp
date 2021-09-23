b259345c5769b6c5018c42206068550369e3e07d
Aidan Cuennet <aidan.cuennet@gmail.com>
2021-09-23 15:37:16 +0200

    added finished PokerGame

--- formative 0.1-0.2 - PokerGame/formative 0.1-0.2 - PokerGame/formative 0.1-0.2 - PokerGame.cpp
+++ formative 0.1-0.2 - PokerGame/formative 0.1-0.2 - PokerGame/formative 0.1-0.2 - PokerGame.cpp
@@ -1,153 +1,154 @@
 #include <iostream>
 #include <string>
+#include <array>
 
 enum class Value : int
 {
-	Two = 2,
-	Three = 3,
-	Four = 4,
-	Five = 5,
-	Six = 6,
-	Seven = 7,
-	Eight = 8,
-	Nine = 9,
-	Ten = 10,
-	Jack,
-	Queen,
-	King,
-	Ace
-	
+    Two = 2,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace
 };
 
 enum class Suit : int
 {
-	Spades,
-	Diamonds,
-	Hearts,
-	Clubs
+    Spades = 1,
+    Coeur,
+    Carreau,
+    Clubs
 };
 
-struct Card
-{
-	Value value;
-	Suit suit;
+struct Card {
+    Value value;
+    Suit suit;
+
 };
 
-std::string printValue(Value value_)
-{
-	switch (value_)
-	{
-	case Value::Two:
-		return "two";
-		break;
-		{
-	case Value::Three:
-		return "three";
-		break;
-		}
-		{
-	case Value::Four:
-		return "four";
-		break;
-		}
-		{
-	case Value::Five:
-		return "five";
-		break;
-		}
-		{
-	case Value::Six:
-		return "six";
-		break;
-	}
-		{
-	case Value::Seven:
-		return "seven";
-		break;
-		}
-		{
-	case Value::Eight:
-		return "eight";
-		break;
-		}
-		{
-	case Value::Nine:
-		return "nine";
-		break;
-		}
-		{
-	case Value::Ten:
-		return "ten";
-		break;
-		}
-		{
-	case Value::Jack:
-		return "jack";
-		break;
-		}
-		{
-	case Value::Queen:
-		return "queen";
-		break;
-		}
-		{
-	case Value::King:
-		return "king";
-		break;
-		}
-		{
-	case Value::Ace:
-		return "ace";
-		break;
-		}
-	default:
-		return "default";
-		break;
+std::string printValues(Value value_) {
 
-	}
+    switch (value_)
+    {
+    case Value::Two:
+        return "Two";
+        break;
+    case Value::Three:
+        return "Three";
+        break;
+    case Value::Four:
+        return "Four";
+        break;
+    case Value::Five:
+        return "Five";
+        break;
+    case Value::Six:
+        return "Six";
+        break;
+    case Value::Seven:
+        return "Seven";
+        break;
+    case Value::Eight:
+        return "Eight";
+        break;
+    case Value::Nine:
+        return "Nine";
+        break;
+    case Value::Ten:
+        return "Ten";
+        break;
+    case Value::Jack:
+        return "Jack";
+        break;
+    case Value::Queen:
+        return "Queen";
+        break;
+    case Value::King:
+        return "King";
+        break;
+    case Value::Ace:
+        return "Ace";
+        break;
+    default:
+        return "Default";
+        break;
+    }
+
 }
 
-std::string printSuit(Suit suit_)
-{
-	
-	switch(suit_)
-	{
-	case Suit::Spades:
-		return "spades";
-		break;
-	case Suit::Diamonds:
-		return "diamonds";
-		break;
-	case Suit::Hearts:
-		return "hearts";
-		break;
-	case Suit::Clubs:
-		return "clubs";
-		break;
-	default:
-		return "default";
-		break;
-	}
+std::string printSuits(Suit suit_) {
+
+    switch (suit_)
+    {
+    case Suit::Spades:
+        return "Spades";
+        break;
+    case Suit::Coeur:
+        return "Hearts";
+        break;
+    case Suit::Carreau:
+        return "Diamonds";
+        break;
+    case Suit::Clubs:
+        return "Clubs";
+        break;
+    default:
+        return "Default";
+        break;
+    }
+
 }
-std::string printCards(Card card_ )
-{
-	return printValue(card_.value) + " of " + printSuit(card_.suit);
-	
+
+std::string printCards(Card card_) {
+    return printValues(card_.value) + " of " + printSuits(card_.suit);
 }
 
 int main()
 {
-	for (int t = 4; t <= 4; t++)
-	{
-		for (int v = 14; v <= 14; v++)
-		{
-			Card myCard;
-			myCard.suit = static_cast<Suit>(5);
-			myCard.value = static_cast<Value>(9);
+    std::array<Card, 52> deck{};
+    int deckDeal = 0;
 
-			std::cout << printCards(myCard) << std::endl;
-		}
-	}
-	
+    for (int s{ 1 }; s <= 4; s++) {
+
+        for (int v{ 2 }; v <= 14; v++) {
+
+            Card myCard;
+            myCard.suit = static_cast<Suit>(s);
+            myCard.value = static_cast<Value>(v);
+
+            deck.at(deckDeal) = myCard;
+            deckDeal++;
+
+        }
+    }
+
+
+    srand(time(NULL));
+    std::array<int, 5> cardIndexes{};
+    cardIndexes.fill(-1);
+
+    for (int n{ 0 }; n < 5; n++) {
+        int idxCardTemp = rand() % 52;
+
+        bool trouve = false;
+        for (auto iCard : cardIndexes) {
+            if (iCard == idxCardTemp) {
+                trouve = true;
+                break;
+            }
+        }
+
+        if (!trouve) {
+            cardIndexes.at(n) = idxCardTemp;
+            std::cout << printCards(deck.at(idxCardTemp)) << std::endl;
+        }
+    }
+
 }
-
