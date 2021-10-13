bf94ffa48da72825ec6dfe78769070f5353490da
Aidan Cuennet <aidan.cuennet@gmail.com>
2021-10-13 02:08:29 +0200

    added continued orchard (WIP)

--- CPlusPlus-Class/04 - Exercice 7 - OOP - Orchard/04 - Exercice 7 - OOP - Orchard/appleTree.cpp
+++ CPlusPlus-Class/04 - Exercice 7 - OOP - Orchard/04 - Exercice 7 - OOP - Orchard/appleTree.cpp
@@ -1,1 +1,13 @@
 #include "appleTree.h"
+
+appleTree::appleTree() : Tree()
+{
+	fruitWeight = 0.150f;
+	initFruitQuantity();
+
+}
+
+void appleTree::initFruitQuantity()
+{
+	nbFruits = 600 + rand() % 250;
+}
